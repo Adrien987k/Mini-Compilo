fcddca2fe93a4d57c367e56d2befeb9b8ecb3a78
ADRIEN BARDES <adrien.bardes@etu.u-pec.fr>
2017-10-19 22:10:50 +0200

    Prints

--- compilo.ml
+++ compilo.ml
@@ -6,30 +6,67 @@ type boolexpr = Var of string
 let env = [] (*  (string * int)  *)
 
 let compile_aux expr env =
+  let nb_var = List.length env in
   let output = [] in
-  let output = ".data
-say:
-         .string \"Variable values:\\n\"
-.align 8
-var:
-.string \"  %s: %d\\n\"
-.align 8
-res:
-.string \"Result: %d\\n\"
-.align 8
-               "
+  let output = (".data\n" ^
+               "\tsay:\n" ^
+               "\t.string \"Variable values:\\n\"\n" ^
+               "\t.align 8\n" ^
+               "var:\n" ^
+               "\t.string \"  %s: %d\\n\"\n" ^
+               "\t.align 8\n" ^
+               "res:\n" ^
+               "\t.string \"Result: %d\\n\"\n" ^
+               "\t.align 8\n")
                :: output in
   let output = List.fold_left (fun acc var -> let name, _  = var in
-                          ((" " ^ name ^ ":
-                                  .asciz \"" ^ name ^ "\"
-                                  .align 8\n")
+                          ((name ^ ":\n" ^
+                            "\t.asciz \"" ^ name ^ "\"\n" ^
+                            "\t.align 8\n")
                           :: acc)) output env in
-  let output = ".text
-.global main
-main:
-.type   main, @function\n" :: output in
+  let output = (".text\n" ^
+               "\t.global main\n" ^
+               "main:\n" ^
+               "\t.type   main, @function\n" ^
+               "\tpushq   %rbp\n" ^
+               "\tmovq   %rsp, %rbp\n") 
+               :: output in
+  let bytes = nb_var * 16 in
+  let output = ("\tsubq   $" ^ (string_of_int bytes) ^ ", %rsp\n") 
+               :: output in
+  let rec collect_args n offset out =
+    if n = 0 then out else
+    let out = ("\tmovq   24(%rbp), %rax\n" ^
+              "\tmovq   " ^ (string_of_int offset) ^ "(%rax), %rax\n" ^
+              "\tmovq   %rax, %rdi\n" ^
+              "\tmovq   $0, %rax\n" ^
+              "\tcall   atoi\n" ^
+              "\tmovq   %rax, " ^ (string_of_int (n * (-8))) ^ "(%rbp)\n")
+              :: out in
+    collect_args (n - 1) (offset + 8) out
+  in
+  let output = collect_args nb_var 8 output in
+  let output = ("\tmovq   $say, %rdi\n" ^
+               "\tmovq   $0, %rax\n" ^
+               "\tcall   printf\n") 
+               :: output in
+  let out = (output, nb_var) in
+  let out = 
+    List.fold_left (fun acc var -> let name, _  = var in
+                                   let out, n   = acc in
+      (((
+        "\tmovq   " ^ (string_of_int (n * (-8))) ^ "(rbp), %rax\n" ^
+        "\tmovq   %rax, 16(%rsp)\n" ^
+        "\tmovq   %rax, %rdx\n" ^
+        "\tmovq   $" ^ name ^ ", %rsi\n" ^
+        "\tmovq   $var, %rdi\n" ^
+        "\tmovq   $0, %rax\n" ^
+        "\tcall   printf\n"
+      )
+      :: out), (n - 1))) out env in
+  let output, _ = out in
   let output = List.rev output in
   let output = String.concat "" output in
   Printf.printf "%s" output
 
-let _ = compile_aux (Var ("")) (["toto", 5; "tata", 7])
+let _ = compile_aux (Var ("")) (["toto", 5; "tata", 7; "var", 2])
