1c457e3c6a70a085c29c94a39f49dbefc1e2d4f5
ADRIEN BARDES <adrien.bardes@etu.u-pec.fr>
2017-10-20 12:42:53 +0200

    Almost working

--- compilo.ml
+++ compilo.ml
@@ -3,10 +3,12 @@ type boolexpr = Var of string
               | And of boolexpr * boolexpr
               | Not of boolexpr
 
-let env = [] (*  (string * int)  *)
-
 let compile_aux expr env =
   let nb_var = List.length env in
+  let offset_argv offset = 
+    let bytes = (nb_var + 1) * 8 in
+    bytes - (- (offset))
+  in
   let output = [] in
   let output = (".data\n" ^
                "\tsay:\n" ^
@@ -19,11 +21,12 @@ let compile_aux expr env =
                "\t.string \"Result: %d\\n\"\n" ^
                "\t.align 8\n")
                :: output in
-  let output = List.fold_left (fun acc var -> let name, _  = var in
-                          ((name ^ ":\n" ^
-                            "\t.asciz \"" ^ name ^ "\"\n" ^
-                            "\t.align 8\n")
-                          :: acc)) output env in
+  let output = 
+    List.fold_left (fun acc var -> let name, _  = var in
+                     ((name ^ ":\n" ^
+                       "\t.asciz \"" ^ name ^ "\"\n" ^
+                       "\t.align 8\n")
+                     :: acc)) output env in
   let output = (".text\n" ^
                "\t.global main\n" ^
                "main:\n" ^
@@ -34,28 +37,25 @@ let compile_aux expr env =
   let bytes = nb_var * 16 in
   let output = ("\tsubq   $" ^ (string_of_int bytes) ^ ", %rsp\n") 
                :: output in
-  let rec collect_args n offset out =
-    if n = 0 then out else
-    let out = ("\tmovq   24(%rbp), %rax\n" ^
-              "\tmovq   " ^ (string_of_int offset) ^ "(%rax), %rax\n" ^
-              "\tmovq   %rax, %rdi\n" ^
-              "\tmovq   $0, %rax\n" ^
-              "\tcall   atoi\n" ^
-              "\tmovq   %rax, " ^ (string_of_int (n * (-8))) ^ "(%rbp)\n")
-              :: out in
-    collect_args (n - 1) (offset + 8) out
-  in
-  let output = collect_args nb_var 8 output in
+  let output = 
+    List.fold_left (fun acc var -> let name, offset = var in
+      ((
+        "\tmovq   24(%rbp), %rax\n" ^
+        "\tmovq   " ^ (string_of_int (offset_argv offset)) ^ "(%rax), %rax\n" ^
+        "\tmovq   %rax, %rdi\n" ^
+        "\tmovq   $0, %rax\n" ^
+        "\tcall   atoi\n" ^
+        "\tmovq   %rax, " ^ (string_of_int offset) ^ "(%rbp)\n"
+      )
+      :: acc)) output env in
   let output = ("\tmovq   $say, %rdi\n" ^
                "\tmovq   $0, %rax\n" ^
-               "\tcall   printf\n") 
+               "\tcall   printf\n")
                :: output in
-  let out = (output, nb_var) in
-  let out = 
-    List.fold_left (fun acc var -> let name, _  = var in
-                                   let out, n   = acc in
-      (((
-        "\tmovq   " ^ (string_of_int (n * (-8))) ^ "(rbp), %rax\n" ^
+  let output = 
+    List.fold_left (fun acc var -> let name, offset = var in
+      ((
+        "\tmovq   " ^ (string_of_int offset) ^ "(rbp), %rax\n" ^
         "\tmovq   %rax, 16(%rsp)\n" ^
         "\tmovq   %rax, %rdx\n" ^
         "\tmovq   $" ^ name ^ ", %rsi\n" ^
@@ -63,8 +63,7 @@ let compile_aux expr env =
         "\tmovq   $0, %rax\n" ^
         "\tcall   printf\n"
       )
-      :: out), (n - 1))) out env in
-  let output, _ = out in
+      :: acc)) output env in
   let rec compile_expr expr out op alone =
     match expr with
     | Var s ->
@@ -73,7 +72,7 @@ let compile_aux expr env =
                               (String.compare name s) = 0) env
         in
         let _, offset = var in
-        let out = 
+        let out =
         ("\tmovq   " ^ (string_of_int offset) ^ "(%rbp), %rax\n")
         :: out in
         if not alone then 
@@ -96,21 +95,44 @@ let compile_aux expr env =
                         :: out
                       else
                         out
-    | Not e -> if op then (* Not is not working ! *)
-                    let out = (compile_expr e [] false true) @ out in 
-                    ("\tnotq   %rbx\n" ^
-                    "\tpushq   %rax\n")
-                    :: out 
-               else let out = (compile_expr e [] false true) @ out in
-                    ("\tnotq   %rbx\n" ^
-                    "\tpopq   %rbx\n")
-                    :: out
+    | Not e -> let out = (compile_expr e [] false true) @ out in 
+               let out = "\tnotq   %rax\n" :: out in
+               if not alone then
+                 (if op then "\tpushq   %rax\n" else "\tpopq   %rbx\n")
+                 :: out
+               else
+                 out
   in
   let output = compile_expr expr output true true in
+  let output = ("\tmovq   %rax, %rsi\n" ^
+                "\tmovq   $res, %rdi\n" ^
+                "\tmovq   $0, %rax\n" ^
+                "\tcall   printf\n" ^
+                "\tmovq   8(%rsp), %rax\n" ^
+                "\tcall   exit"
+               ) :: output in
   let output = List.rev output in
   let output = String.concat "" output in
-  Printf.printf "%s" output
+  Printf.printf "%s\n" output
 
-(* TODO réecrire la partie 1 avec offset + affichage *)
+(* 
+let _ = compile_aux (Not(And(Var ("x1"), Not(Var ("x2")))))
+        (["x1", (-32); 
+          "x2", (-24); 
+          "x3", (-16);
+          "x4", (-8)])
+*)
 
-let _ = compile_aux (And(Var ("x1"),Var ("x2"))) (["x1", (-24); "x2", (-16); "x3", (-8)])
+let compile expr = 
+  let rec collect_vars expr vars = 
+    match expr with
+    | Var s -> s :: vars
+    | Or (e1, e2)
+    | And (e1, e2) -> (collect_vars e1 []) @ (collect_vars e2 vars)
+    | Not e -> collect_vars e vars
+  in
+  let vars = collect_vars expr [] in
+  let vars = List.mapi (fun i var -> var, (i + 1) * (-8)) vars in
+  compile_aux expr vars
+
+let _ = compile (Not(And(Var ("x1"), Not(Var ("x2")))))
